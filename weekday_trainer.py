7d8e208c77384194f40f2700af3c867fec0d6cfd
Phillip Eckert <phillip.eckert@ezag.de>
2023-09-21 20:39:51 -0400

    timer added

--- weekday_trainer.py
+++ weekday_trainer.py
@@ -1,5 +1,14 @@
 import random
+import sys
+import threading
+import time
+
 ########## Functions
+def custom_input(prompt):
+    user_input = input(prompt)
+    if user_input.lower() in ["break", "exit"]:
+        sys.exit("Program terminated by user.")
+    return user_input
 def manual_weekday(date):
     """
     Goal: My own version of the weekday calculator. It proceeds in the same way as I do in my head. First the month, then the month and the day.
@@ -33,7 +42,15 @@ def manual_weekday(date):
     if int(year) % 4 == 0 and (int(month) == 1 or int(month) == 2):
         weekday = weekdays_index[rest-1]
     return weekday
-
+def highscore_loader():
+    try:
+        # Attempt to read the 'level' value from the file 'level_data.txt'
+        with open("highscore.txt", "r") as file:
+            highscore = int(file.read())
+    except FileNotFoundError:
+        # If the file is not found, set a default value for 'level'
+        highscore = 1  # Set a default value (change this to your desired default)
+    return highscore
 def auto_weekday(date):
     """
     Goal: Use the datetime library to automatically tell the weekday
@@ -50,8 +67,8 @@ def auto_weekday(date):
         return weekdays[weekday_number]
     except ValueError:
         return "Invalid date format. Please enter in the format dd.mm.yyyy."
-
 def generate_random_date(year_start, year_end):
+
     day = random.randint(1, 28)  # You can adjust the range of days as needed
     month = random.randint(1, 12)
     year = random.randint(int(year_start), int(year_end))  # You can adjust the range of years as needed
@@ -62,25 +79,23 @@ def generate_random_date(year_start, year_end):
     year_str = str(year)
 
     date_string = f"{day_str}.{month_str}.{year_str}"
+
     return date_string
 
+    while True:
+        elapsed_time = time.time() - start_time
+        mins, secs = divmod(int(elapsed_time), 60)
+        sys.stdout.write(f"\rTime elapsed: {mins:02}:{secs:02}   ")  # Overwrite the previous line
+        sys.stdout.flush()
+        time.sleep(1)  # Update every second
 def application():
     print("_________")
     print("WEEKDAY TRAINER")
     print("_________")
     print("Choose a start and end year and then calculate the given date in your head. Insert your result. Try to max the Streak!") 
-    # Load Highscore
-    try:
-        # Attempt to read the 'level' value from the file 'level_data.txt'
-        with open("highscore.txt", "r") as file:
-            highscore = int(file.read())
-    except FileNotFoundError:
-        # If the file is not found, set a default value for 'level'
-        highscore = 1  # Set a default value (change this to your desired default)
-
-    print("Your highscore is: " + str(highscore))
-    year_start = input("START YEAR: ")
-    year_end = input("END YEAR: ")
+    print("Your highscore is: " + str(highscore_loader()))
+    year_start = custom_input("START YEAR: ")
+    year_end = custom_input("END YEAR: ")
     level = 1
     # The Game
     while True:
@@ -88,19 +103,34 @@ def application():
         print("_________")
         print("LEVEL " + str(level))
         print("DATE IS: " + date)
-        answer = input("Weekday: ")
+        start_time = time.time()
+        answer = custom_input("Weekday: ")
+
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 20:  # 30 seconds
+            print("\nTime's up!")
+            print(elapsed_time)
+            break
+
         if answer == manual_weekday(date):
             print("Right!")
-            reset = False
+            print(elapsed_time)
+
             level = level + 1
         elif answer != manual_weekday(date):
             print("Wrong! It was a " + manual_weekday(date))
             break
     print("ENDE! You reached Level: " + str(level))
-    print("Your highscore is: " + str(highscore))
+    print("Your highscore is: " + str(highscore_loader()))
+    highscore = int(highscore_loader())
     if level > highscore:
         highscore = level
         with open("highscore.txt", "w") as file:
             file.write(str(highscore))
 
-application()
+input = custom_input("Do you want to train (1) or play (2)")
+if input == 1:
+    train()
+elif input == 0:    
+    application()
+
